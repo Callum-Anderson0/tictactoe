da020623958e72ed0db038b3dec0f16c4dce53ab
Callum Anderson <cm20cja@leeds.ac.uk>
2025-02-25 13:15:09 +0000

    Added draw notification functionality

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -20,21 +20,34 @@ function TicTacToe(){
         for (let combo of winningCombos) {
           const [a, b, c] = combo;
           if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
-            setWinner(gameState[a]);
+            setWinner(gameState[a] + " WINS!");
             return;
           }
         }
       };
+
+    const checkForDraw = () => { 
+        let count = 0;
+        gameState.forEach((tile) => {
+            if (tile === ""){
+                count+=1;
+            }
+        });
+        if(count === 0 && winner === ""){
+            setWinner("DRAW")
+        }
+    }
     
 
     useEffect(() => {
         checkForWinner();
+        checkForDraw();
     },[gameState]);
     
     
     const handleClick = (id) => {
         if(gameState[id] === ""){
-            if(winner == ""){
+            if(winner === ""){
                 const newState = [...gameState];
                 newState[id] = player;
                 setGamestate(newState);
@@ -53,7 +66,7 @@ function TicTacToe(){
 
     return (
             <div className="flexbox justify-center w-full md:w-[768px] bg-gray-900 rounded-md shadow-lg">
-                <div className="bg-gray-800 mt-2 mb-2 bottom-4 w-full h-8 text-gray-300 rounded-md">
+                <div className="bg-gray-800 mt-2 mb-2 bottom-4 w-full h-8 text-gray-300 rounded-md shadow-lg">
                     <h1 className="text-lg mt-2 text-center text-bold">NOUGHTS AND CROSSES</h1>
                 </div>
                 <div className="flex justify-center">
@@ -61,7 +74,7 @@ function TicTacToe(){
                 </div>
                 {winner &&
                  <div className="fixed top-1/2 left-1/2 -translate-x-1/2 text-center -translate-y-1/2 bg-white shadow-lg p-6 rounded-lg w-64">
-                    <p>{winner} WINS!</p>
+                    <p>{winner}</p>
                     <button className="text-center mt-2 h-8 w-full text-gray-300 bg-gray-800 rounded-md shadow-lg" onClick={handleReset}> RESET</button>
                  </div>
                 }
